d03e9d268809b642b1e460b123d79c4c6ca5daf4
Luis E Avila <54752453+luisd159@users.noreply.github.com>
2023-11-16 13:09:50 -0500

    cors

--- prueba_tecnica/obj/Debug/net8.0/prueba_tecnica.AssemblyInfo.cs
+++ prueba_tecnica/obj/Debug/net8.0/prueba_tecnica.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("prueba_tecnica")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1267842474d9eb304dd2ea01b3e484d965579718")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f13e5b67b0474221fd791e277c7f9987deebe63")]
 [assembly: System.Reflection.AssemblyProductAttribute("prueba_tecnica")]
 [assembly: System.Reflection.AssemblyTitleAttribute("prueba_tecnica")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
